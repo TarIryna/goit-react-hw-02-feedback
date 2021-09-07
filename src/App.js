dd75b8741f60c29b7b635bbf18233b6cac5b7cc2
Tar Iryna <tar86irina@gmail.com>
2021-09-08 00:14:07 +0300

    added task1

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import './App.css';
+import React, { Component } from 'react';
+import Controls from './components/Controls';
+import Statistics from './components/Statistics';
+
+class App extends Component {
+
+   state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+   };
+  
+    addGood = () => {
+        this.setState(prevState => ({
+          good: prevState.good + 1,
+        }));
+    };
+    addNeutral = () => {
+        this.setState(prevState => ({
+            neutral: prevState.neutral + 1,
+        }));
+    };
+    addBad = () => {
+        this.setState(prevState => ({
+          bad: prevState.bad + 1,
+        }));
+    };
+
+  countTotal = () => {
+    const total = this.state.good + this.state.neutral + this.state.bad;
+    return total;
+  };
+
+  countPositivePercentage = () => {
+    let percentage = 0;
+    if (this.state.good > 0) {
+      return percentage = this.state.good / this.countTotal() * 100;
+    }
+  }
+
+  render() {
+    const { good, neutral, bad} = this.state;
+  return (
+    <div className="App">
+      <Controls
+        onGood={this.addGood}
+        onNeutral={this.addNeutral}
+        onBad={this.addBad}
+      />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={this.countTotal()}
+        percentage={ this.countPositivePercentage()}
+      />
+    </div>
+  );
+}
+}
+
+export default App;
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Statistics.module.css';
+
+const Statistics = ({good, neutral, bad, total, percentage}) => (
+     <div className="container">
+        <h2 className={s.title}>Statisctics</h2>
+            <p className={ s.statInfo}>Good: {good}</p>
+            <p className={ s.statInfo}>Neutral: {neutral}</p>
+            <p className={s.statInfo}>Bad: {bad}</p>
+            <p className={s.statInfo}>Total: {total}</p>
+            <p className={s.statInfo}>Positive Feedback: {percentage}%</p>
+    </div>
+);
+
+export default Statistics;
