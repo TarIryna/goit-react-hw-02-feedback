6d511394b3fed9218c353bc1bef92718b3f1a0c0
Tar Iryna <tar86irina@gmail.com>
2021-09-09 10:42:38 +0300

    proprypes added

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import s from './Controls.module.css';
 
 const Controls = ({statNames, onBtnFeedback}) => (
--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import s from './Notification.module.css';
 
 const Notification = ({ message }) => (
